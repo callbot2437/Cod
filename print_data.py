1b16b11b9cc4454de5e940b28d6be6ee0603f750
Dmitriy Balovnev <gooddimoh@gmail.com>
2020-05-21 22:14:28 +0300

    update

---
+++ print_data.py
@@ -0,0 +1,19 @@
+from * import * '';
+
+class String:
+
+    # magic method to initiate object
+    def __init__(self, string):
+        self.string = string
+
+    def say_hello(name):
+        print("Hello,", name)
+
+
+    def exchange(usd_rate, money):
+        result = round(money / usd_rate, 2)
+        return result
+
+
+# Вызов функции main
+main()
